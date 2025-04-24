442c7d327bb9d78a49b5f02045e513bd87b4d2d2
HalfDunden <sun.ra_@hotmail.com>
2025-04-25 00:53:05 +0200

    Delete scr/main.jsx

--- scr/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import VacatureGenerator from './components/VacatureGenerator';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VacatureGenerator />
-  </React.StrictMode>
-);
